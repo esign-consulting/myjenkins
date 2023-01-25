c6cd67529c20938dcb9df542c2f65491a5298068
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2023-01-25 22:23:57 +0000

    Setting AdminWhitelistRule no longer has any effect

--- scripts/harden-jenkins.groovy
+++ scripts/harden-jenkins.groovy
@@ -1,16 +1,12 @@
 #!groovy
 
 import jenkins.model.Jenkins
-import jenkins.security.s2m.*
 import hudson.security.csrf.DefaultCrumbIssuer
 
 Jenkins jenkins = Jenkins.getInstance()
 
 // CSRF protection
 jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true))
-
-// Enable Agent to master security subsystem
-jenkins.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
 
 // Disable old Non-Encrypted protocols
 HashSet<String> newProtocols = new HashSet<>(jenkins.getAgentProtocols());
